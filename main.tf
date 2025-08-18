81eb836b78543a98d158ad99a16b7d9e3ed0b81f
J D <jd@Js-MacBook-Air.local>
2025-08-18 23:21:41 +0100

    First Terraform commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+# provider Configuration
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
